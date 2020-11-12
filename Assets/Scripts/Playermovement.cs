6d55459f7c051eb4fbb0667241ccb599febd992a
urkkiz <urkkiz225@gmail.com>
2020-11-12 19:33:24 +0200

    projekti jatkuu yha

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Playermovement : MonoBehaviour
 {
+
+    public Rigidbody2D myRigidBody;
+    public BoxCollider2D myBody;
+
+    public float horizontalMovement = 0f;
+    public float moveSpeed = 5f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myBody = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        horizontalMovement = Input.GetAxis("Horizontal");
+
+        float flipX = Input.GetAxisRaw("Horizontal");
+
+        if (flipX != 0)
+        {
+           FlipPlayer(flipX);
+        }
+    }
+
+    public void FlipPlayer(float x)
+    {
         
+    }
+
+    private void FixedUpdate()
+    {
+       myRigidBody.velocity = new Vector2(horizontalMovement * moveSpeed, myRigidBody.velocity.y);
     }
 }
